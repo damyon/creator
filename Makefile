877346a473e8d7eaff90377904a437cd516ecdfb
Damyon Wiese <damyon.wiese@gmail.com>
2025-04-01 22:04:47 +0800

    Redirect

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	wasm-pack build --target web --out-dir docs
+	wasm-pack build --target web --out-dir app
 
 dev: build
-	php -S 0.0.0.0:8080 -t docs/
+	php -S 0.0.0.0:8080 -t app/
---
+++ app/creator.d.ts
@@ -0,0 +1,58 @@
+/* tslint:disable */
+/* eslint-disable */
+export function init_scene(): boolean;
+export function scene_names(): Promise<any>;
+export function save_scene(): Promise<any>;
+export function load_scene(): Promise<boolean>;
+export function delete_scene(): Promise<boolean>;
+export function set_scene_name(name: string): boolean;
+export function load_first_scene(): Promise<any>;
+export function set_material_color(red: string, green: string, blue: string): boolean;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly init_scene: (a: number) => void;
+  readonly scene_names: () => number;
+  readonly save_scene: () => number;
+  readonly load_scene: () => number;
+  readonly delete_scene: () => number;
+  readonly set_scene_name: (a: number, b: number, c: number) => void;
+  readonly load_first_scene: () => number;
+  readonly set_material_color: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_export_4: WebAssembly.Table;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h6ac027c7b4e47d5f: (a: number, b: number, c: number, d: number) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h123f9824b6b43e3d: (a: number, b: number) => void;
+  readonly _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8553e69a8c5040fa: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h660e6c10043db219: (a: number, b: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h96d32d8e879cf7f9: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h6e645cd760448ecb: (a: number, b: number, c: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke2_mut__he629759d72d74ba1: (a: number, b: number, c: number, d: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
